f8c1fa007bb07b69476779f8a5958ebb1ffdb170
nebkat <webmaster@nebkat.com>
2011-04-12 19:21:42 +0100

    Add 'scaling_available_frequencies' for SMParts CPU Settings

--- Kernel/drivers/cpufreq/cpufreq.c
+++ Kernel/drivers/cpufreq/cpufreq.c
@@ -684,6 +684,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_governor.attr,
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
+    &cpufreq_freq_attr_scaling_available_freqs,
 	&scaling_setspeed.attr,
 	NULL
 };
